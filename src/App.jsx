e5dac7a2f617c8f7c05e5f98cbdb9ebfd07905d1
nitheeshx86 <knitheesh0360@gmail.com>
2025-09-03 09:14:18 +0530

    v3 - navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 // App.jsx
 import React from 'react';
+import Navbar from './navbar';
 import Todo from './todo';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Todo />
     </div>
   );
